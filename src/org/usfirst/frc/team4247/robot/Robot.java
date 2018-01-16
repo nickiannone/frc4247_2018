722a3a953b1cab3acfec4a8697f81c4609c368a2
skillsusa <skillsusa@10.192.4.84>
2018-01-16 17:31:41 -0600

    Added first compilable version of code.

---
+++ src/org/usfirst/frc/team4247/robot/Robot.java
@@ -0,0 +1,46 @@
+package org.usfirst.frc.team4247.robot;
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+
+public class Robot extends IterativeRobot {
+	
+	@Override
+	public void robotInit() {
+		// Initialize all the things!
+	}
+	
+	@Override
+	public void robotPeriodic() {
+		// Compressor?
+	}
+	
+	@Override
+	public void disabledInit() {
+		// Disable the bot
+	}
+	
+	@Override
+	public void disabledPeriodic() {
+		// Do something cool with lights and stuff, I dunno
+	}
+	
+	@Override
+	public void autonomousInit() {
+		// Start me up, and never stop
+	}
+	
+	@Override
+	public void autonomousPeriodic() {
+		// Magical AI bullcrap
+	}
+	
+	@Override
+	public void teleopInit() {
+		// Go go go!
+	}
+	
+	@Override
+	public void teleopPeriodic() {
+		// Drive and stuff
+	}
+}
