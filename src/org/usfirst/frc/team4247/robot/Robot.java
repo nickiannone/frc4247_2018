1b9de82404d10ebc99771931ab2052c276c620b8
Nicholas Iannone <Nicholas.Iannone@MacGregorPartners.com>
2018-01-20 02:52:20 -0600

    Added initial version of Autonomous code.

--- src/org/usfirst/frc/team4247/robot/Robot.java
+++ src/org/usfirst/frc/team4247/robot/Robot.java
@@ -1,12 +1,41 @@
 package org.usfirst.frc.team4247.robot;
 
+import org.usfirst.frc.team4247.robot.autonomous.AutoState;
+import org.usfirst.frc.team4247.robot.autonomous.Driver;
+import org.usfirst.frc.team4247.robot.autonomous.FieldMap;
+import org.usfirst.frc.team4247.robot.autonomous.Navigator;
+import org.usfirst.frc.team4247.robot.autonomous.State;
+import org.usfirst.frc.team4247.robot.autonomous.Task;
+import org.usfirst.frc.team4247.robot.vision.VisionProcessor;
+
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Timer;
 
 public class Robot extends IterativeRobot {
 	
+	// TODO Robot controls
+	private Timer timer = new Timer();
+	//...
+	
+	// Vision values
+	private VisionProcessor vision;
+	
+	// Autonomous values
+	private State state = State.START;
+	private boolean enteringState = true;
+	private int frameCounter;
+	
+	public Navigator navigator;
+	public Driver driver;
+	public FieldMap fieldMap;
+	
 	@Override
 	public void robotInit() {
-		// Initialize all the things!
+		// Set up the controls
+		
+		// Set up vision processing
+		this.vision = new VisionProcessor();
 	}
 	
 	@Override
@@ -26,12 +55,60 @@ public class Robot extends IterativeRobot {
 	
 	@Override
 	public void autonomousInit() {
-		// Start me up, and never stop
+		// Move into START state
+		this.state = State.START;
+		this.enteringState = true;
+		
+		// Start the timer
+		this.timer.start();
+		this.frameCounter = 0;
+		
+		// Get the GSC from the field, and reinitialize the FieldMap
+		String gsc = DriverStation.getInstance().getGameSpecificMessage();
+		this.fieldMap = new FieldMap(gsc);
+		
+		// Set up the Navigator and the Driver
+		this.navigator = new Navigator(this);
+		this.driver = new Driver(this);
 	}
 	
 	@Override
 	public void autonomousPeriodic() {
-		// Magical AI bullcrap
+		this.frameCounter++;
+		if (this.enteringState) {
+			Task[] tasks = this.navigator.generateInitialTasks(this.state);
+			this.driver.setTasks(tasks);
+		} else {
+			// Get the position updates from the driver
+			AutoState as = this.driver.getAutoState(this.frameCounter);
+			
+			// Let the navigator see the state, and 
+			this.navigator.updateFieldMap(this.fieldMap, as);
+			
+			// If we need to find a new plan
+			if (as.needsReevaluation) {
+				Task[] tasks = this.navigator.reevaluateTasks(this.state, as);
+				this.driver.setTasks(tasks);
+				return;
+			}
+			
+			// If we need to run vision processing
+			if (as.needsVisionProcessing) {
+				// TODO Do vision processing, see if we have a cube!
+				this.driver.setVisionResults(this.vision.processVision());
+				return;
+			}
+			
+			// If we're done with this state, move on.
+			if (as.stateComplete) {
+				this.state = as.nextState;
+				this.enteringState = true;
+				return;
+			}
+			
+			// Otherwise, just let the driver do what they do.
+			this.driver.executeTasks();
+		}
 	}
 	
 	@Override
--- src/org/usfirst/frc/team4247/robot/autonomous/Driver.java
+++ src/org/usfirst/frc/team4247/robot/autonomous/Driver.java
@@ -1,5 +1,8 @@
 package org.usfirst.frc.team4247.robot.autonomous;
 
+import org.usfirst.frc.team4247.robot.Robot;
+import org.usfirst.frc.team4247.robot.vision.VisionResult;
+
 /**
  * The Driver class takes the place of a traditional driver
  * during Autonomous mode. By being fed a series of Tasks, it
@@ -9,5 +12,34 @@ package org.usfirst.frc.team4247.robot.autonomous;
  * @author NickIannone
  */
 public class Driver {
+	
+	private Task[] currentTasks;
+	private VisionResult visionResult;
+
+	public Driver(Robot robot) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void setTasks(Task[] tasks) {
+		this.currentTasks = tasks;
+	}
+
+	public AutoState getAutoState(int frameCounter) {
+		// Called at the start of a frame of autonomous.
+		// Generates a state object encapsulating anything needed for this frame.
+
+		// TODO Finish this!
+		return null;
+	}
+
+	public void setVisionResults(VisionResult visionResult) {
+		// Called whenever we get vision results
+		this.visionResult = visionResult;
+	}
+
+	public void executeTasks() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
